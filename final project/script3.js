e4653780c1eb2795da04a7af28a80203f80324aa
مراحب عدنان يوسف - 577 <marahebx001@gmail.com>
2020-07-19 19:28:32 +0300

    done

---
+++ final project/script3.js
@@ -0,0 +1,30 @@
+// alert(
+// "get ready to explore our kuwait <3");
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+// Get the modal
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+alert("Thank you for signing in ")
